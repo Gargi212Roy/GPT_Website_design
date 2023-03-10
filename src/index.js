99ce2eb6758bc93d9cda4bb7bb80ff3d30848216
itsgargiroy <itsgargiroy@gmail.com>
2023-03-10 12:46:43 +0530

    basic formatting and css done

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// used to hook our index.js to index.html
+
+import App from "./App";
+import "./index.css";
+ReactDOM.render(<App />, document.getElementById("root"));
